fe722222c25e7a29e6e79f2eda470d54889a24e2
filipecsoares <filipecarlossoares@gmail.com>
2024-09-01 10:51:34 -0300

    test: create test to update candidate

--- election-management/src/test/java/infrastructure/resource/CandidateResourceTest.java
+++ election-management/src/test/java/infrastructure/resource/CandidateResourceTest.java
@@ -6,10 +6,12 @@ import static org.mockito.Mockito.*;
 
 import api.CandidateApi;
 import api.dto.in.CreateCandidate;
+import api.dto.in.UpdateCandidate;
 import api.dto.out.Candidate;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.MediaType;
 import org.instancio.Instancio;
 import org.jboss.resteasy.reactive.RestResponse;
@@ -48,5 +50,38 @@ class CandidateResourceTest {
         verifyNoMoreInteractions(api);
 
         assertEquals(out, Arrays.stream(response).toList());
+    }
+
+    @Test
+    void update() {
+        var id = "123";
+        var in = Instancio.create(UpdateCandidate.class);
+        var out = Instancio.create(Candidate.class);
+
+        when(api.update(id, in)).thenReturn(out);
+
+        var response = given().contentType(MediaType.APPLICATION_JSON).body(in)
+                .when().put("/{id}", id)
+                .then().statusCode(RestResponse.StatusCode.OK).extract().as(Candidate.class);
+
+        verify(api).update(id, in);
+        verifyNoMoreInteractions(api);
+
+        assertEquals(out, response);
+    }
+
+    @Test
+    void updateNonExistentCandidate() {
+        var id = "non-existent-id";
+        var in = Instancio.create(UpdateCandidate.class);
+
+        when(api.update(id, in)).thenThrow(new NotFoundException());
+
+        given().contentType(MediaType.APPLICATION_JSON).body(in)
+                .when().put("/{id}", id)
+                .then().statusCode(RestResponse.StatusCode.NOT_FOUND);
+
+        verify(api).update(id, in);
+        verifyNoMoreInteractions(api);
     }
 }
