510efce468eac6c6af2a9a10b5e34bd7b03decfb
filipecsoares <filipecarlossoares@gmail.com>
2024-09-01 17:53:31 -0300

    feat: create result service

--- election-management/src/main/java/api/dto/out/Election.java
+++ election-management/src/main/java/api/dto/out/Election.java
@@ -8,7 +8,7 @@ public record Election(String id, List<Candidate> candidates) {
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     public record Candidate(String id,
             Optional<String> photo,
-            String fullName,
+            String name,
             String email,
             Optional<String> phone,
             Optional<String> jobTitle,
